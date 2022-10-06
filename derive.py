88f4744721062853963dad53fe39f22a6b3a370b
Riley Johnson <riley.scott.johnson@gmail.com>
2022-10-05 20:14:40 -0400

    Dictionary complete

--- derive.py
+++ derive.py
@@ -4,9 +4,17 @@ N = 3
 fileName = sys.argv[-1]
 file = open(fileName, 'r')
 wordDict = {}
+nonTerminals = []
 
 
 if len(sys.argv) == 3:
     N = sys.argv[1]
 
+
+for line in file:
+    args = line.strip().split(' ')
+    if args[0] in wordDict.keys():
+        wordDict[args[0]] = [wordDict[args[0]], args[2:]]
+    else:
+        wordDict[args[0]] = args[2:]
 
