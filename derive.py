3e44cfd6e462a98458b197e0b7ee4d365f4a3f11
Riley Johnson <riley.scott.johnson@gmail.com>
2022-10-05 20:32:10 -0400

    Allow for command-line args to be in any order

--- derive.py
+++ derive.py
@@ -1,20 +1,28 @@
 import sys
 
 N = 3
-fileName = sys.argv[-1]
-file = open(fileName, 'r')
+fileName = None
 wordDict = {}
 nonTerminals = []
 
 
 if len(sys.argv) == 3:
-    N = sys.argv[1]
+    if sys.argv[1][0:2] == "-l":
+        N = sys.argv[1]
+        fileName = sys.argv[-1]
+    else:
+        N = sys.argv[-1]
+        fileName = sys.argv[1]
+
+file = open(fileName, 'r')
 
 
 for line in file:
     args = line.strip().split(' ')
+    nonTerminals.append(args[0])
     if args[0] in wordDict.keys():
         wordDict[args[0]] = [wordDict[args[0]], args[2:]]
     else:
         wordDict[args[0]] = args[2:]
+
 
